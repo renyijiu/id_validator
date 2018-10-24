067030e46b4a3373523665dfcaa242261bb222c9
jxlwqq <jxlwqq@gmail.com>
2018-10-24 15:21:16 +0800

    [refactor] generate_order_code function

--- lib/id_validator/concern/helper.rb
+++ lib/id_validator/concern/helper.rb
@@ -218,8 +218,8 @@ module IdValidator
       def generate_order_code(sex)
         order_code = rand(1..999)
 
-        order_code = order_code % 2 == 0 ? order_code : order_code - 1 if sex == 0
-        order_code = order_code % 2 == 0 ? order_code - 1 : order_code if sex == 1
+        if sex != order_code % 2
+          order_code -= 1
 
         Func.get_str_pad(order_code, 3, '0', false)
       end
