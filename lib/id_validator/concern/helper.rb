cf93d607b96fdf0c3fa9f42de2ece459d9a02418
renyijiu <lanyuejin1108@gmail.com>
2018-10-25 18:42:37 +0800

    [feat]update get_address_info

--- lib/id_validator/concern/helper.rb
+++ lib/id_validator/concern/helper.rb
@@ -51,6 +51,15 @@ module IdValidator
       def get_random_birthday_code
         Time.at(rand * Time.now.to_i).strftime('%Y%m%d')
       end
+
+      # 获取指定区划代码最新的地址名称
+      def get_address_info(address_code)
+        address_code = address_code.to_i
+        address_arr = IdValidator::Config.total_address_info.fetch(address_code, [])
+
+        address = address_arr.sort_by{|a| a[:end_year]}.last || {}
+        address.fetch(:address, nil)
+      end
     end
 
     module Helper
@@ -81,26 +90,15 @@ module IdValidator
 
       # 获取地址信息
       def get_address_info(address_code)
-        address_code_hash = IdValidator::Config.address_info
-        abandoned_address_code_hash = IdValidator::Config.abandoned_address_code
+        province_address_code = "#{address_code[0...2]}0000"
+        city_address_code = "#{address_code[0...4]}00"
 
-        province_address_code = "#{address_code[0...2]}0000".to_i
-        city_address_code = "#{address_code[0...4]}00".to_i
-
-        province = address_code_hash[province_address_code] || abandoned_address_code_hash[province_address_code]
-
-        if address_code[0].to_i != 8
-          city = address_code_hash[city_address_code] || abandoned_address_code_hash[city_address_code]
-          district = address_code_hash[address_code.to_i] ||abandoned_address_code_hash[address_code.to_i]
-        else
-          city = nil
-          district = nil
-        end
+        is_mainland = (address_code[0].to_i != 8)
 
         {
-            province: province,
-            city: city,
-            district: district
+            province: Func.get_address_info(province_address_code),
+            city: is_mainland ? Func.get_address_info(city_address_code) : nil,
+            district: is_mainland ? Func.get_address_info(address_code) : nil
         }
       end
 
